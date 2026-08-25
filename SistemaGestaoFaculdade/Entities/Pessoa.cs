96df73c345f184a658d8b813a41af666f352783a
Rosana Lima <128074155+ROSANARRL@users.noreply.github.com>
2026-08-25 17:34:51 -0300

    Implementa cadastro de pessoas (#5)

---
+++ SistemaGestaoFaculdade/Entities/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModuloPOO.DesafioSquad.Models
+{
+    public abstract class Pessoa
+    {
+        //Propriedades comuns a qualquer pessoa no sistema
+        public string Nome { get; set; } = string.Empty;
+        public string Cpf {  get; set; } = string.Empty;
+        public string Email {  get; set; } = string.Empty;
+
+        //Exibe dados básicos (pode ser sobrescrito pelas classes filhos)
+        public virtual void ExibirDados()
+        {
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"CPF: {Cpf}");
+            Console.WriteLine($"E-mail: {Email}");
+        }
+    }
+}
