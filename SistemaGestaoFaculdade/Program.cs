68caecf4b050ad93fcb418914ba245b0e68dcedf
Bianca Fernandes <bianca.fernandesoliv@gmail.com>
2026-08-25 02:31:59 -0300

    Implementa cadastro e consulta de cursos

--- SistemaGestaoFaculdade/Program.cs
+++ SistemaGestaoFaculdade/Program.cs
@@ -1,1 +1,43 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SistemaGestaoFaculdade.Entities;
+using SistemaGestaoFaculdade.Enums;
+using SistemaGestaoFaculdade.Services;
+
+SistemaFaculdade sistema = new SistemaFaculdade();
+
+/* OPÇÃO 1 DO MENU - CADASTRAR CURSO
+
+Console.WriteLine("\n============================");
+Console.WriteLine("     Cadastro de curso      ");
+Console.WriteLine("============================");
+
+Console.Write("\nInforme o código do curso: ");
+string codigoCurso = Console.ReadLine();
+
+Console.Write("Informe o nome do curso: ");
+string nomeCurso = Console.ReadLine();
+
+Console.Write("Informe o tipo do curso (1 - Graduação | 2 - Pós Graduação): ");
+
+string tipoDigitado = Console.ReadLine();
+
+if (!int.TryParse(tipoDigitado, out int tipoSelecionado)) throw new ArgumentException("Tipo de curso informado é inválido.");
+
+TipoCurso tipoCurso = (TipoCurso)tipoSelecionado;
+
+Curso curso = new Curso(codigoCurso, nomeCurso, tipoCurso);
+
+sistema.CadastrarCurso(curso);
+*/
+
+
+/* OPÇÃO 9 DO MENU - CONSULTAR CURSOS
+
+Console.WriteLine("\n============================");
+Console.WriteLine("      Consultar cursos      ");
+Console.WriteLine("============================");
+
+sistema.ConsultarCursos();
+
+*/
+
+
---
+++ SistemaGestaoFaculdade/Services/SistemaFaculdade.cs
@@ -0,0 +1,37 @@
+﻿using SistemaGestaoFaculdade.Entities;
+using SistemaGestaoFaculdade.Enums;
+
+namespace SistemaGestaoFaculdade.Services {
+    public class SistemaFaculdade {
+
+        public List<Curso> Cursos { get; set; } = new();
+        public List<Disciplina> Disciplinas { get; set; } = new();
+
+        public void CadastrarCurso(Curso curso) {
+
+            if (Cursos.Any(x => x.Codigo == curso.Codigo)) throw new ArgumentException("Já existe um curso cadastrado com esse código!");
+
+            Cursos.Add(curso);
+        }
+
+        public void ConsultarCursos() {
+
+            foreach (var curso in Cursos) {
+
+                Console.WriteLine("\n------ Curso ------");
+                Console.WriteLine($"Nome: {curso.Nome.ToUpper()} - {curso.Codigo.ToUpper()}");
+                Console.WriteLine($"Tipo: {(curso.Tipo == TipoCurso.Graduacao ? "Graduação" : "Pós-Graduação")}");
+
+                /*
+
+                Console.WriteLine("\nDisciplinas:");
+
+                IMPLEMENTAR!!!
+
+                Console.WriteLine("\nAlunos matriculados:");
+
+                */
+            }
+        }
+    }
+}
