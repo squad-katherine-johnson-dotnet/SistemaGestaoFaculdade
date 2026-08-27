b109c9a6cc0dfb9f2a03fd3f682e560602b44ab9
Yasmin <yasb367@gmail.com>
2026-08-27 00:13:01 -0300

    implementa casdastro de disciplina e vinculação de disciplina com o curso

--- SistemaGestaoFaculdade/Services/SistemaFaculdade.cs
+++ SistemaGestaoFaculdade/Services/SistemaFaculdade.cs
@@ -1,24 +1,45 @@
 ﻿using SistemaGestaoFaculdade.Entities;
 using SistemaGestaoFaculdade.Enums;
 
-namespace SistemaGestaoFaculdade.Services {
-    public class SistemaFaculdade {
+namespace SistemaGestaoFaculdade.Services
+{
+    public class SistemaFaculdade
+    {
 
         public List<Curso> Cursos { get; set; } = new();
-        public List<Disciplina> Disciplinas { get; set; } = new(); 
+        public List<Disciplina> Disciplinas { get; set; } = new();
         public List<Aluno> Alunos { get; set; } = new();
         public List<Professor> Professores { get; set; } = new();
 
-        public void CadastrarCurso(Curso curso) {
+        public void CadastrarCurso(Curso curso)
+        {
 
             if (Cursos.Any(x => x.Codigo == curso.Codigo)) throw new ArgumentException("Já existe um curso cadastrado com esse código!");
 
             Cursos.Add(curso);
         }
 
-        public void ConsultarCursos() {
+        public void CadastrarDisciplina(Disciplina disciplina)
+        {
+            if (Disciplinas.Any(x => x.Codigo == disciplina.Codigo))
+                throw new ArgumentException("Já existe uma disciplina cadastrada com esse código");
 
-            foreach (var curso in Cursos) {
+            Disciplinas.Add(disciplina);
+        }
+
+        public void VincularDisciplinaCurso(Curso curso, Disciplina disciplina)
+        {
+            if (curso.Disciplinas.Any(x => x.Codigo == disciplina.Codigo))
+                throw new ArgumentException("Essa disciplina já está vinculada a esse curso");
+
+            curso.Disciplinas.Add(disciplina);
+        }
+
+        public void ConsultarCursos()
+        {
+
+            foreach (var curso in Cursos)
+            {
 
                 Console.WriteLine("\n-------------- Curso --------------");
                 Console.WriteLine($"Nome: {curso.Nome.ToUpper()} - {curso.Codigo.ToUpper()}");
