c51cbd3cd43750eee65ef26ee5d0accc490864fb
mariana-mbs <mariana-mbs@outlook.com>
2026-08-29 14:45:58 -0300

    Implementa regras de notas e boletim

--- SistemaGestaoFaculdade/Entities/Boletim.cs
+++ SistemaGestaoFaculdade/Entities/Boletim.cs
@@ -8,5 +8,41 @@
         {
             Notas = new List<Nota>();
         }
+
+        public void AdicionarNota(Nota nota)
+        {
+            if (nota is null)
+                throw new ArgumentNullException(
+                    nameof(nota),
+                    "A nota deve ser informada."
+                );
+
+            if (Notas.Any(x =>
+                x.Disciplina.Codigo == nota.Disciplina.Codigo))
+            {
+                throw new ArgumentException(
+                    "Já existe uma nota lançada para essa disciplina."
+                );
+            }
+
+            Notas.Add(nota);
+        }
+
+        public Nota? BuscarNotaPorDisciplina(string codigoDisciplina)
+        {
+            if (string.IsNullOrWhiteSpace(codigoDisciplina))
+            {
+                throw new ArgumentException(
+                    "O código da disciplina deve ser informado."
+                );
+            }
+
+            return Notas.FirstOrDefault(x =>
+                x.Disciplina.Codigo.Equals(
+                    codigoDisciplina,
+                    StringComparison.OrdinalIgnoreCase
+                )
+            );
+        }
     }
 }
