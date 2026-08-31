4362586975e18d75a8792dcbf86d2e458c6f6c04
Bianca Fernandes <bianca.fernandesoliv@gmail.com>
2026-08-31 15:49:41 -0300

    Corrige erro na execução do sistema

--- SistemaGestaoFaculdade/Entities/Nota.cs
+++ SistemaGestaoFaculdade/Entities/Nota.cs
@@ -1,30 +1,22 @@
 ﻿using SistemaGestaoFaculdade.Enums;
 
-namespace SistemaGestaoFaculdade.Entities
-{
-    public class Nota
-    {
+namespace SistemaGestaoFaculdade.Entities {
+    public class Nota {
         public Disciplina Disciplina { get; private set; }
         public double Valor { get; private set; }
 
-        public Nota(Disciplina disciplina, double valor)
-        {
+        public Nota(Disciplina disciplina, double valor) {
             if (disciplina is null)
-                throw new ArgumentNullException(
-                    nameof(disciplina),
-                    "A disciplina deve ser informada."
-                );
+                throw new ArgumentNullException(nameof(disciplina), "A disciplina deve ser informada.");
 
             if (valor < 0 || valor > 10)
                 throw new ArgumentException("A nota deve estar entre 0 e 10.");
-                );
 
             Disciplina = disciplina;
             Valor = valor;
         }
 
-        public bool EstaAprovado(TipoCurso tipoCurso)
-        {
+        public bool EstaAprovado(TipoCurso tipoCurso) {
             if (tipoCurso == TipoCurso.Graduacao)
                 return Valor >= 7;
 
